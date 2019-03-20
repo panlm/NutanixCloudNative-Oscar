c226fddb51256b19ae2064c31d8d7e4bc216d534
stevenpan <stevenpan@stevenpans-MacBook-Pro.local>
2019-03-21 00:51:08 +0800

    db config in settings_local.py

---
+++ sandbox/settings_local.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'abc',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '10.139.82.93',
+        'PORT': '',
+    }
+}
